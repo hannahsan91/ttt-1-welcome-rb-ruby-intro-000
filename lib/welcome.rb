b14ea39680e9a8577ef9fe2bc59eeed2e878d35f
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-04-24 21:40:46 +0000

    Automatically backed up by Learn

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,7 +1,6 @@
 learn# Edit this file to output "Welcome to Tic Tac Toe!"
 puts "Welcome to Tic Tac Toe!
 learn
-learn
 lib/welcome.rb
 ruby lib/welcome.rb
 "# ruby lib/welcome.rb from your terminal.
